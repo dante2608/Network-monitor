19d5551b536eef9376bc9dab52df5675c253894d
Dat Tran <63tran@cua.edu>
2021-02-08 20:40:29 -0500

    Add Boiost as dependency

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,17 @@
+#include <boost/asio.hpp>
+#include <boost/system/error_code.hpp>
+
 #include <iostream>
 
 int main()
 {
-    std::cout << "It works" << std::endl;
-    return 0;
+    // A 'falsey' error_code means "no error".
+    boost::system::error_code ec {};
+    if (ec) {
+        std::cerr << "Error: " << ec.message() << std::endl;
+        return -1;
+    } else {
+        std::cout << "OK" << std::endl;
+        return 0;
+    }
 }
