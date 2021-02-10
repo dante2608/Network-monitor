69a4e3610951772c44db129d010cd7713abd9fe7
Dat Tran <63tran@cua.edu>
2021-02-10 14:28:23 -0500

    Connect to ws://echo.websocket.org (sync)

--- src/main.cpp
+++ src/main.cpp
@@ -1,55 +1,84 @@
-#include <boost/beast/core.hpp>
-#include <boost/beast/websocket.hpp>
-#include <boost/asio/connect.hpp>
-#include <boost/asio/ip/tcp.hpp>
-#include <cstdlib>
+#include <boost/asio.hpp>
+#include <boost/beast.hpp>
+#include <boost/system/error_code.hpp>
+
+#include <iomanip>
 #include <iostream>
 #include <string>
 
-namespace beast = boost::beast;         // from <boost/beast.hpp>
-namespace http = beast::http;           // from <boost/beast/http.hpp>
-namespace websocket = beast::websocket; // from <boost/beast/websocket.hpp>
-namespace net = boost::asio;            // from <boost/asio.hpp>
-using tcp = boost::asio::ip::tcp;       // from <boost/asio/ip/tcp.hpp>
+using tcp = boost::asio::ip::tcp;
+namespace websocket = boost::beast::websocket;
+
+void Log(const std::string& where, boost::system::error_code ec)
+{
+    std::cerr << "[" << std::setw(20) << where << "] "
+              << (ec ? "Error: " : "OK")
+              << (ec ? ec.message() : "")
+              << std::endl;
+}
 
 int main()
 {
-    std::string host = "echo.websocket.org";
-    auto const port = "80";
-    auto const text = "Hello, world";
+    // Connection targets
+    const std::string url {"echo.websocket.org"};
+    const std::string port {"80"};
+    const std::string message {"Hello WebSocket"};
 
-    // The io_context is required for all I/O
-    net::io_context ioc;
+    // Always start with an I/O context object.
+    boost::asio::io_context ioc {};
 
-    // These objects perform our I/O
-    tcp::resolver resolver{ioc};
-    websocket::stream<tcp::socket> ws{ioc};
+    // Under the hood, socket.connect uses I/O context to talk to the socket
+    // and get a response back. The response is saved in ec.
+    boost::system::error_code ec {};
 
-    // Look up the domain name
-    auto const results = resolver.resolve(host,port);
+    // Resolve the endpoint.
+    tcp::resolver resolver {ioc};
+    auto endpoint {resolver.resolve(url, port, ec)};
+    if (ec) {
+        Log("resolver.resolve", ec);
+        return -1;
+    }
 
-    // Connect the TCP socket
-    net::connect(ws.next_layer(), results);
+    // Connect the TCP socket.
+    tcp::socket socket {ioc};
+    socket.connect(*endpoint, ec);
+    if (ec) {
+        Log("socket.connect", ec);
+        return -2;
+    }
 
-    // Perform the websocket handshake
-    ws.handshake(host, "/");
+    // Tie the socket object to the WebSocket stream and attempt an handshake.
+    websocket::stream<boost::beast::tcp_stream> ws {std::move(socket)};
+    ws.handshake(url, "/", ec);
+    if (ec) {
+        Log("ws.handshake", ec);
+        return -3;
+    }
 
-    // Send the message
-    ws.write(net::buffer(std::string(text)));
+    // Tell the WebSocket object to exchange messages in text format.
+    ws.text(true);
 
-    // This buffer will hold the incoming message
-    beast::flat_buffer buffer;
+    // Send a message to the connected WebSocket server.
+    boost::asio::const_buffer wbuffer {message.c_str(), message.size()};
+    ws.write(wbuffer, ec);
+    if (ec) {
+        Log("ws.write", ec);
+        return -4;
+    }
 
-    // Read a message into our buffer
-    ws.read(buffer);
+    // Read the echoed message back.
+    boost::beast::flat_buffer rbuffer {};
+    ws.read(rbuffer, ec);
+    if (ec) {
+        Log("ws.read", ec);
+        return 51;
+    }
 
-    // Close the WebSocket connection
-    ws.close(websocket::close_code::normal);
+    // Print the echoed message.
+    std::cout << "ECHO: "
+              << boost::beast::make_printable(rbuffer.data())
+              << std::endl;
 
-    // If we get here then the connection is closed gracefully
-
-    // The make_printable() function helps print a ConstBufferSequence
-    std::cout << beast::make_printable(buffer.data()) << std::endl;
-
+    Log("returning", ec);
     return 0;
 }
